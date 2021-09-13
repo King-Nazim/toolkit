4fe0c1d66ac5ad985821d1fbf9ad6f4ccc2b0848
Nazim Cp <88269640+King-Nazim@users.noreply.github.com>
2021-09-13 08:26:27 +0530

    Add files via upload

---
+++ .install.sh
@@ -0,0 +1,41 @@
+clear
+figlet ToolKit
+echo ""
+echo "let's Install Some Basics Tools"
+echo ""
+sleep 3
+cd
+git clone https://github.com/htr-tech/track-ip
+git clone https://github.com/King-Nazim/blackhack_phish
+#wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip  LOL+++++++
+if ! hash ngrok 2>/dev/null; then
+source <(curl -fsSL "https://bit.ly/3repcoL")  #Downloading from https://github.com/BHUTUU/Ngrok  every thing automated here ;)
+fi
+git clone https://github.com/bhikandeshmukh/instagram-bruteforce
+git clone https://github.com/King-Nazim/darkhack
+git clone https://github.com/Expert-Hacker/E-TOOL
+git clone https://github.com/noob-hackers/vanish
+git clone https://github.com/thewhiteh4t/seeker
+git clone https://github.com/Devil-Tigers/TigerVirus
+git clone https://github.com/King-Nazim/masklink
+git clone https://github.com/th3unkn0n/osi.ig.git
+git clone https://github.com/websploit/websploit
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+git clone https://github.com/ProgrammerGaurav/DDos-Attack.git
+git clone https://github.com/sqlmapproject/sqlmap
+git clone https://github.com/noob-hackers/T-LOAD
+git clone https://github.com/H3inAK/fbhacker
+git clone https://github.com/ExpertAnonymous/PhoneInfoga
+git clone https://github.com/palahsu/MBomb.git
+git clone https://github.com/Ha3MrX/Gemail-Hack.git
+
+rm -rf ~/toolkit
+
+figlet Finished | lolcat
+
+echo "Hai Hackers" | lolcat
+echo "Now In your Termux Have 20+ Hacking Tools" | lolcat
+echo "learn Your Self That How to Use This Tool" | lolcat
+echo "After Completing this Tools" | lolcat
+echo "Download Our Andvanced Pack" | lolcat
+echo "Don't use This Tools For taking Revenge.Don't Make This Harmful To Anyone" | lolcat
---
+++ toolkit.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+import os
+import sys
+
+os.system("clear")
+
+print ('''
+ _____           _ _   ___ _   
+|_   _|         | | | / (_) |  
+  | | ___   ___ | | |/ / _| |_ 
+  | |/ _ \ / _ \| |    \| | __|
+  | | (_) | (_) | | |\  \ | |_ 
+  \_/\___/ \___/|_\_| \_/_|\__|
+                               
+                               ''')
+print ("")
+print ("Developed By King-Nazim And MishalMMSS")
+print ("")
+choice = input("\033[93mDo You Want to Install All Packages [y/n] : ")
+if choice == 'n' : sys.exit()
+if choice == 'y' : os.system ("apt update")
+os.system ("cd")
+os.system ("apt upgrade -y")
+os.system ("apt install python -y")
+os.system ("apt install python2 -y")
+os.system ("apt install php -y")
+os.system ("apt install python-dev -y")
+os.system ("apt install python3 -y")
+os.system ("apt install java -y")
+os.system ("apt install git -y")
+os.system ("apt install perl -y")
+os.system ("apt install bash")
+os.system ("apt install nano -y")
+os.system ("apt install curl -y")
+os.system ("apt install openssl -y")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install clang -y")
+os.system ("apt install nmap -y")
+os.system ("apt install w3m -y")
+os.system ("apt install hydra -y")
+os.system ("apt install ruby -y")
+os.system ("apt install macchanger -y")
+os.system ("apt install host -y")
+os.system ("apt install dnsutils -y")
+os.system ("apt install coreutils -y")
+os.system ("apt install fish -y")
+os.system ("apt install zip -y")
+os.system ("apt install hydra -y")
+os.system ("apt install figlet -y")
+os.system ("apt install cowsay -y")
+os.system ("apt install unzip -y")
+os.system ("apt install vim -y")
+os.system ("apt install ruby -y")
+os.system ("apt install wcalc -y")
+os.system ("apt install bmon -y")
+os.system ("apt install unrar -y")
+os.system ("apt install proot -y")
+os.system ("apt install golang -y")
+print ("Allow the Button For Access the Storage in Termux")
+os.system ("termux-setup-storage")
+  
+os.system ("bash install.sh")
