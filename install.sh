de24f2ae7c1903f52f1d16eab4f32e7ffca46d79
Nazim Cp <88269640+King-Nazim@users.noreply.github.com>
2021-09-20 20:22:35 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,68 @@
+clear
+figlet ToolKit
+echo ""
+echo "let's Install Some Tools"
+echo ""
+sleep 3
+cd
+git clone https://github.com/htr-tech/track-ip
+git clone https://github.com/King-Nazim/blackhack_phish
+#wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip  LOL+++++++
+if ! hash ngrok 2>/dev/null; then
+source <(curl -fsSL "https://bit.ly/3repcoL")  #Downloading from https://github.com/BHUTUU/Ngrok  every thing automated here ;)
+fi
+git clone https://github.com/bhikandeshmukh/instagram-bruteforce
+git clone https://github.com/King-Nazim/darkhack
+git clone https://github.com/Expert-Hacker/E-TOOL
+git clone https://github.com/noob-hackers/vanish
+git clone https://github.com/thewhiteh4t/seeker
+git clone https://github.com/Devil-Tigers/TigerVirus
+git clone https://github.com/King-Nazim/masklink
+git clone https://github.com/th3unkn0n/osi.ig.git
+git clone https://github.com/websploit/websploit
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+git clone https://github.com/ProgrammerGaurav/DDos-Attack.git
+git clone https://github.com/sqlmapproject/sqlmap
+git clone https://github.com/noob-hackers/T-LOAD
+git clone https://github.com/H3inAK/fbhacker
+git clone https://github.com/ExpertAnonymous/PhoneInfoga
+git clone https://github.com/palahsu/MBomb.git
+git clone https://github.com/Ha3MrX/Gemail-Hack.git
+git clone https://github.com/th3unkn0n/TeleGram-Scraper.git
+git clone https://github.com/khansaad1275/FotoSploit 
+git clone https://github.com/Manisso/fsociety.git 
+git clone https://github.com/TermuxHackz/wifi-hacker
+git clone https://github.com/waseem-sajjad/WifiGod
+wget https://raw.github.com/derv82/wifite/master/wifite.py
+git clone https://github.com/threat9/routersploit
+git clone https://github.com/i3visio/osrframework
+git clone https://github.com/Mebus/cupp
+git clone https://github.com/faizann24/wifi-bruteforcer-fsecurify
+git clone https://github.com/openwall/john
+git clone https://github.com/OWASP/QRLJacking/tree/master/QRLJacker
+git clone https://github.com/LionSec/xerosploit
+git clone https://github.com/4shadoww/hakkuframework
+git clone https://github.com/savio-code/fern-wifi-cracker.git
+git clone https://github.com/Zack-sys/A-Rat-Exploit
+wget Auxilus.github.io/metasploit.sh
+git clone https://github.com/D4Vinci/PyLoggy.git
+curl -LO https://raw.githubusercontent.com/Hax4us/setoolkit/master/setoolkit.sh 
+git clone https://github.com/offensive-security/exploitdb
+git clone https://github.com/reyammer/shellnoob
+git clone https://github.com/farinap5/A-xex
+git clone https://github.com/screetsec/Brutal
+git clone https://github.com/beefproject/beef
+git clone https://github.com/Ciphey/Ciphey
+
+
+rm -rf ~/toolkit
+
+
+figlet Finished | lolcat
+
+echo "Hai Hackers" | lolcat
+echo "Now In your Termux Have 20+ Hacking Tools" | lolcat
+echo "learn Your Self That How to Use This Tool" | lolcat
+echo "After Completing this Tools" | lolcat
+echo "Download Our Andvanced Pack" | lolcat
+echo "Don't use This Tools For taking Revenge.Don't Make This Harmful To Anyone" | lolcat
